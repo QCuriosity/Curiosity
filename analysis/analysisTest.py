ce050cfe4e3b3b80a1a9ab4707d4111563edf2e1
Ender <wanjiadong@gmail.com>
2012-12-24 02:28:40 +0800

    update analysisTest.py

--- analysis/analysisTest.py
+++ analysis/analysisTest.py
@@ -1,6 +1,21 @@
 from analysis import * 
+import MySQLdb
+import sys
 
-sentences = ["I'm wan jiadong", "My name is &er sjf", "He name is aa%aa"]
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+db = MySQLdb.connect('localhost', 'root', 'password', 'test', charset = 'utf8')
+cur = db.cursor()
+
+query = "SELECT text FROM statuses"
+cur.execute(query)
+rows = cur.fetchall()
+sentences = []
+for r in rows:
+    if (type(r[0]) == type(u' ')):
+        sentences.append(r[0])
+    
 ana = analysis(sentences)
 
 ana.splitAllSentences()
