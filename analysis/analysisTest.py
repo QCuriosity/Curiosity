19724a46353e88a644dc7310e1d7e7ce396c32c6
Ender <wanjiadong@gmail.com>
2012-12-26 21:29:00 +0800

    update analysis

--- analysis/analysisTest.py
+++ analysis/analysisTest.py
@@ -1,21 +1,27 @@
 from analysis import * 
 import MySQLdb
 import sys
+import io
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-db = MySQLdb.connect('localhost', 'root', 'password', 'test', charset = 'utf8')
+db = MySQLdb.connect('localhost', 'root', 'password', 'curiosity', charset = 'utf8')
 cur = db.cursor()
 
-query = "SELECT text FROM statuses"
+query = "SELECT text FROM status_t"
+#query = "SELECT text FROM status_t limit 0, 10000"
 cur.execute(query)
 numrow = int(cur.rowcount)
 ana = analysis()
-for r in xrange(numrow):
+for i in xrange(numrow):
+    r = cur.fetchone()
     if (type(r[0]) == type(u' ')):
         ana.addAnalysisSentence(r[0])
+db.close()
     
+f = io.open('result3', 'w')
+for k in ana.wordRate.keys():
+    f.write(k + ': ' + str(ana.wordRate[k]) + '\n')
 
-
-print ana.wordRate
+f.close()
